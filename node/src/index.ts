8575fd3668a56f55d8da72c1dbdfc17115552375
ajuvercr <arthur.vercruysse@outlook.com>
2025-03-06 16:39:50 +0100

    add node bindings

---
+++ node/src/index.ts
@@ -0,0 +1,12 @@
+export * from "./generated/service";
+export { LogMessage } from "./generated/log";
+export {
+  Close,
+  DataChunk,
+  Error,
+  Id,
+  Message,
+  StreamMessage,
+} from "./generated/common";
+export { Processor, RunnerMessage } from "./generated/runner";
+export { Empty } from "./generated/google/protobuf/empty";
